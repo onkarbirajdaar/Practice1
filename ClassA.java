0f9d3d03a6a61a2d12abc4e7a7c9c95d0675e2f4
onkarbirajdaar <onkarbirajdar098@gmail.com>
2025-06-18 21:11:50 +0530

    Java class committed

---
+++ ClassA.java
@@ -0,0 +1,12 @@
+public class ClassA
+{
+    public void meth1()
+    {
+        System.out.println("Writing First line in github repo");
+    }
+    public static void main(String []args)
+    {
+        ClassA aobj=new ClassA();
+        aobj.meth1();
+    }
+}
